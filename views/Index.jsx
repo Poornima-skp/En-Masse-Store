271f4f3cf0f36963e31b261285ce9784a782c85d
poornima.skp <poornima.skp@gmail.com>
2022-05-24 12:09:16 -0500

    Add New Product link works

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,20 +6,33 @@ class Index extends React.Component{
     render() {
         const Product  = this.props.product;
         return(
+            
            <DefaultLayout title = "Kitchen Products">
-                <div class="productPage">  
+                <div className='add'>
+                    <a href="/products/new">ADD New Product</a>
+                </div>
+                <div class="productPage"> 
+                     
                         {Product.map(product => {
                             return(
-                                <div key ={product._id} class="product">
-                                    
-                                    <a id='productName' href={`/products/${product._id}`}>{product.name}</a><br />
-                                    <a href={`/products/${product._id}`}><img src={product.image} alt="" /></a><br />
-                                    <div className="productDetails">
-                                        {'Price: $'}{product.price} <br />
-                                        {'Quantity: '}{product.quantity} <br />
-                                         <h4>{product.quantity > 0 ? 'In Stock' : 'Out Of Stock!'}</h4>
+                                
+                              
+                                   
+                                    <div key={product._id} class="product">
+
+                                        <a id='productName' href={`/products/${product._id}`}>{product.name}</a><br />
+                                        <a href={`/products/${product._id}`}><img src={product.image} alt="" /></a><br />
+                                        <div className="productDetails">
+                                            {'Price: $'}{product.price} <br />
+                                            {'Quantity: '}{product.quantity} <br />
+                                            <h4>{product.quantity > 0 ? 'In Stock' : 'Out Of Stock!'}</h4>
+                                        </div>
+                                        <form action="/products" method='POST'>
+
+                                        </form>
                                     </div>
-                                </div>
+                               
+                                
                             )
                         })} 
                 </div>
