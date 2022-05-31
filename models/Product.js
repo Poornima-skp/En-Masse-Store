ca83b6c06b7eb1f09c2c69d90d4d38a7c168054a
poornima.skp <poornima.skp@gmail.com>
2022-05-30 20:22:48 -0500

    Changed image type to String

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    image: { type: Buffer, required: true },
+    // image: { type: Buffer, required: true },
+    image: {type:String, required: true},
     price: { type: String, required: true },
     quantity: { type: Number, required: true },
     productDescription: {
