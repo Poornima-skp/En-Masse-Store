1c3bdfc36eb373debc3a1c8c29bcd0aa2b2a295b
poornima.skp <poornima.skp@gmail.com>
2022-05-30 19:17:14 -0500

    Added Start-Scripts for heroku

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.engine('jsx', require('express-react-views').createEngine());
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+    
 });
 
 mongoose.connection.once('open', () => console.log('Connected to Mongo'));
@@ -34,6 +35,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('I run all the routes');
+    next();
+});
 
 
 
